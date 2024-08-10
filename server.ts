5e9736bc4fdd4ff43ad1a1b1d40f463f9df61ba0
Sara Arshad <96328811+Sara-Arshad@users.noreply.github.com>
2024-08-10 18:33:01 +0500

    Update build command in server.ts

--- server.ts
+++ server.ts
@@ -20,10 +20,13 @@ export function app(): express.Express {
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
-  server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
-    index: 'index.html',
-  }));
+  server.get(
+    '**',
+    express.static(browserDistFolder, {
+      maxAge: '1y',
+      index: 'index.html',
+    })
+  );
 
   // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
@@ -45,11 +48,11 @@ export function app(): express.Express {
 }
 
 function run(): void {
-  const port = process.env['PORT'] || 4000;
+  const port = parseInt(process.env['PORT'] || '4000', 10);
 
   // Start up the Node server
   const server = app();
-  server.listen(port, () => {
+  server.listen(port, '0.0.0.0', () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
 }
